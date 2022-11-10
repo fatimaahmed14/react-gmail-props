254eea1012a8e47387451d9ada959f97fa4342e4
Fatima <fxtimaahmed14@gmail.com>
2022-11-10 10:01:40 +0000

    emails component created

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 import initialEmails from './data/emails'
 
+import EmailList from './components/EmailList'
+
 import './styles/app.css'
 
 const getReadEmails = emails => emails.filter(email => !email.read)
@@ -88,33 +90,9 @@ function App() {
         </ul>
       </nav>
       <main className="emails">
-        <ul>
-          {filteredEmails.map((email, index) => (
-            <li
-              key={index}
-              className={`email ${email.read ? 'read' : 'unread'}`}
-            >
-              <div className="select">
-                <input
-                  className="select-checkbox"
-                  type="checkbox"
-                  checked={email.read}
-                  onChange={() => toggleRead(email)}
-                />
-              </div>
-              <div className="star">
-                <input
-                  className="star-checkbox"
-                  type="checkbox"
-                  checked={email.starred}
-                  onChange={() => toggleStar(email)}
-                />
-              </div>
-              <div className="sender">{email.sender}</div>
-              <div className="title">{email.title}</div>
-            </li>
-          ))}
-        </ul>
+        <EmailList emails= {filteredEmails}
+         toggleRead = {toggleRead}
+         toggleStar ={toggleStar}/>
       </main>
     </div>
   )
