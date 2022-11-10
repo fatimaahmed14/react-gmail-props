643ac6ef5bcaa9c631fdb5009bdfe15f887eded9
Fatima <fxtimaahmed14@gmail.com>
2022-11-10 10:07:54 +0000

    email component created + added to emails

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,31 +1,16 @@
+import Email from "./Email"
+
 function EmailList (props) {
     const {emails , toggleRead, toggleStar} = props
 return (
     <ul>
     {emails.map((email, index) => (
-      <li
+        <Email 
         key={index}
-        className={`email ${email.read ? 'read' : 'unread'}`}
-      >
-        <div className="select">
-          <input
-            className="select-checkbox"
-            type="checkbox"
-            checked={email.read}
-            onChange={() => toggleRead(email)}
-          />
-        </div>
-        <div className="star">
-          <input
-            className="star-checkbox"
-            type="checkbox"
-            checked={email.starred}
-            onChange={() => toggleStar(email)}
-          />
-        </div>
-        <div className="sender">{email.sender}</div>
-        <div className="title">{email.title}</div>
-      </li>
+        email = {email}
+        toggleRead = {toggleRead}
+        toggleStar = {toggleStar}/>
+      
     ))}
   </ul>
 )
